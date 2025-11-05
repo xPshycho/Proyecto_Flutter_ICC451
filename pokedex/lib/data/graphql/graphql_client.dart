9c50ccc81f0ad98b03b5db02dc9443ff5e1ef633
xPshycho <edwinbalbuena189@gmail.com>
2025-11-05 17:12:33 -0400

    feat: agregar permisos de red y mejorar configuración del cliente GraphQL

--- pokedex/lib/data/graphql/graphql_client.dart
+++ pokedex/lib/data/graphql/graphql_client.dart
@@ -4,11 +4,30 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GraphQLService {
   static ValueNotifier<GraphQLClient> initClient() {
-    final HttpLink httpLink = HttpLink('https://beta.pokeapi.co/graphql/v1beta');
+    final HttpLink httpLink = HttpLink(
+      'https://beta.pokeapi.co/graphql/v1beta',
+      defaultHeaders: {
+        'Content-Type': 'application/json',
+      },
+    );
+
+    // Política de reintentos para manejar fallos de red
+    final Link link = httpLink;
 
     final GraphQLClient client = GraphQLClient(
       cache: GraphQLCache(store: InMemoryStore()),
-      link: httpLink,
+      link: link,
+      defaultPolicies: DefaultPolicies(
+        query: Policies(
+          fetch: FetchPolicy.cacheAndNetwork,
+          error: ErrorPolicy.all,
+          cacheRereadPolicy: CacheRereadPolicy.mergeOptimistic,
+        ),
+        mutate: Policies(
+          fetch: FetchPolicy.networkOnly,
+          error: ErrorPolicy.all,
+        ),
+      ),
     );
 
     return ValueNotifier(client);
